c5b8217676f35c53e44f3693c6cb3f97e911e105
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2021-08-09 18:10:45 -0500

    Instalando sass y configurando loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html'
-    })
+      template: './src/index.html',
+    }),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist')
+    contentBase: path.resolve(__dirname, 'dist'),
   },
-   module: {
-     rules: [
-       { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-//       {
-//         test: /\.m?js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//           options: {
-//             presets: ['@babel/preset-env']
-//           }
-//         }
-//       },
-//       {
-//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-//         use: [
-//             {
-//               loader: 'file-loader',
-//             },
-//           ],
-//       },
-//       {
-//         test: /\.html$/i,
-//         loader: "html-loader",
-//       },
-     ]
-   }
-}
-
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
