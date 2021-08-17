63ddea1a2d6071aa829529240a66b08794c54da2
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-08-17 12:41:58 -0500

    Busqueda de la receta con diseño terminado

--- src/js/index.js
+++ src/js/index.js
@@ -8,48 +8,85 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
-let randomBtn = document.querySelector('.random-btn');
-randomBtn.addEventListener('click',()=>{
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then( result => result.json())
-      .then( result => showMeal(result.meals[0]))
+// Receta Random
+// let randomBtn = document.querySelector('.random-btn');
+// randomBtn.addEventListener('click',()=>{
+//     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+//       .then( result => result.json())
+//       .then( result => showMeal(result.meals[0]))
 
-});
+// });
 
 
-const showMeal = (meal) => {
-  const mealTitle = document.querySelector('.meal-title');
-  const recetaImg = document.querySelector('.receta-img');
-  const ingredientes = document.querySelector(".lista-ingredientes");
-  const procedimiento = document.querySelector(".procedimiento");
+// const showMeal = (meal) => {
+//   const mealTitle = document.querySelector('.meal-title');
+//   const recetaImg = document.querySelector('.receta-img');
+//   const ingredientes = document.querySelector(".lista-ingredientes");
+//   const procedimiento = document.querySelector(".procedimiento");
 
-  mealTitle.innerHTML = `${meal.strMeal}`;
-  recetaImg.innerHTML = `<img src="${meal.strMealThumb}" alt="photo">`;
+//   mealTitle.innerHTML = `${meal.strMeal}`;
+//   recetaImg.innerHTML = `<img src="${meal.strMealThumb}" alt="photo">`;
 
-  const ingredientesArray = [];
-  for(let i=0; i<20; i++){
-    let ingrediente = meal[`strIngredient${i}`];
-    let cantidad = meal[`strMeasure${i}`];
-    let imgIngrediente = `https://www.themealdb.com/images/ingredients/${ingrediente}.png`
-    if(ingrediente){
-      ingredientesArray.push([cantidad, ingrediente, imgIngrediente]);
-    }
-  }
-  // console.log(ingredientesArray)
-  ingredientes.innerHTML = ``
-  ingredientesArray.forEach((elemento) => {
-    ingredientes.innerHTML += `
-    <div class="ingrediente">
-<img src="${elemento[2]}"></img>
-<il> ${elemento[0]} ${elemento[1]} </il>
-</div>
-`;
+//   const ingredientesArray = [];
+//   for(let i=0; i<20; i++){
+//     let ingrediente = meal[`strIngredient${i}`];
+//     let cantidad = meal[`strMeasure${i}`];
+//     let imgIngrediente = `https://www.themealdb.com/images/ingredients/${ingrediente}.png`
+//     if(ingrediente){
+//       ingredientesArray.push([cantidad, ingrediente, imgIngrediente]);
+//     }
+//   }
 
-  });
+//   ingredientes.innerHTML = ``
+//   ingredientesArray.forEach((elemento) => {
+//     ingredientes.innerHTML += `
+//     <div class="ingrediente">
+// <img src="${elemento[2]}"></img>
+// <il> ${elemento[0]} ${elemento[1]} </il>
+// </div>
+// `;
 
-  procedimiento.innerHTML =`
-    <h2>Procedimiento</h2>
-    <p>${meal.strInstructions}</p>
-  `
-}
+//   });
 
+//   procedimiento.innerHTML =`
+//     <h2>Procedimiento</h2>
+//     <p>${meal.strInstructions}</p>
+//   `
+// }
+
+
+
+
+let inputSearch = document.getElementById('input-search');
+let btnSearch = document.getElementById('search-btn');
+const mealCard = document.querySelector('.search-result');
+
+btnSearch.addEventListener("click", function () {
+    mealCard.innerHTML = ``
+
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + inputSearch.value, {method: 'GET'})
+    .then(response => response.json())
+    .then(data => {
+
+        mealCard.innerHTML = ``
+        console.log(data.meals)
+        for (const meal of data.meals) {
+
+            mealCard.innerHTML += `
+             <div class="food-item col-lg-2 col-md-3 col-sm-5">
+                <div class="food-image">
+                    <img class="img-fluid" src="${meal.strMealThumb}"
+                        alt="">
+                    <div class="caption">
+                        <div class="blur"></div>
+                        <div class="caption-text">
+                            <i class="fas fa-eye"></i>
+                        </div>
+                    </div>
+                </div>
+                <h4 class="food-name">${meal.strMeal}</h4>
+            </div>
+            `
+        }
+     });
+})
